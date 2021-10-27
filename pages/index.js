c24b85feedfb896cf166813cb43904126d9f3fa3
Christopher <yunchris@gmail.com>
2021-10-27 00:18:02 -0700

    Adjusted svg height

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Christopher from '../components/Christopher'
 import Yun from '../components/Yun'
