acbcfbe05ced1887566fe3e1c2ddccc463a07f82
Christopher <yunchris@gmail.com>
2021-10-30 19:31:29 -0700

    Change relative units to %

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
         <meta charSet="utf-8" />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, user-scalable=no"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
         />
         <meta
           name="keywords"
